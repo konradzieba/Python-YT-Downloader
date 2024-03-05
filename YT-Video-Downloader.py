2a5d104f1f5bb54625dfe063e8fb8a5ab4263d44
Konrad Zięba <162624@student.uwm.edu.pl>
2024-03-05 12:47:44 +0100

    Fix downloading 0B file

--- YT-Video-Downloader.py
+++ YT-Video-Downloader.py
@@ -1,55 +1,20 @@
-import os
 from pytube import YouTube
-from tqdm import tqdm
 
-audio_only: bool = True
+url: str = input("Podaj URL")
+output = input("Audio: [A], Video: [V]")
 
-url: str = input("Podaj URL: ")
-audio_only_input = input("Tylko audio [A], Video [V]: ")
-
-is_input_valid: bool = (
-    isinstance(url, str)
-    and isinstance(audio_only_input, str)
-    and audio_only_input.lower() == "v"
-    or audio_only_input == "a"
+output_format = (
+    "mp4" if output.lower() == "v" else "mp3" if output.lower() == "a" else None
 )
 
-if is_input_valid:
-    if audio_only_input.lower() == "v":
-        audio_only = False
+if output_format is None:
+    raise ValueError("Wpisano niedozwolony znak. Wpisz A lub V")
 
+YouTube(url).streams.first().download()
 
-video = YouTube(url)
+yt = YouTube(url)
+yt.streams.filter(progressive=True, file_extension=output_format).order_by(
+    "resolution"
+).desc().first().download(r"YT_Downloader")
 
-stream = (
-    video.streams.filter(only_audio=audio_only).get_highest_resolution()
-    if not audio_only
-    else video.streams.filter(only_audio=audio_only).first()
-)
-
-file_size = stream.filesize
-
-out_dir = "YT_Downloader"
-
-os.makedirs(out_dir, exist_ok=True)
-
-cleaned_title = "".join(c for c in video.title if c.isalnum() or c in [" ", ".", "-"])
-
-progress_bar = tqdm(total=file_size, unit="B", unit_scale=True, unit_divisor=1024)
-
-
-def on_progress(chunk, file_handle, remaining):
-    if isinstance(chunk, bytes):
-        chunk = len(chunk)
-    progress_bar.update(chunk)
-
-
-stream.on_progress = on_progress
-
-file_format = ".mp4" if not audio_only else ".mp3"
-
-stream.download(output_path=out_dir, filename=f"{cleaned_title}{file_format}")
-
-progress_bar.close()
-
-print(f"{video.title} został pobrany pomyślnie!")
+print(f"\n Pobrano pomyślnie {yt.title}")
